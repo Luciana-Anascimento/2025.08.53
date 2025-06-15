64a58c135a94f832bc22d1ccc84d49d97a587c38
Luciana-Anascimento <lucianna.anascimento@gmail.com>
2025-06-15 04:19:00 -0300

    Update 3.5.2.c

--- atividades/atividade 12-06-25/3.5.2.c
+++ atividades/atividade 12-06-25/3.5.2.c
@@ -1,16 +1,27 @@
 #include<stdio.h>
 
 int main(){
-    int numero;
+    
+    float n1, n2, n3;
 
-    printf("Digite um numero: ");
-    scanf("%d", &numero);
+    printf("Digite o primeiro número: ");
+    scanf("%f", &n1);
+    printf("Digite o segundo número: ");
+    scanf("%f", &n2);
+    printf("Digite o terceiro número: ");
+    scanf("%f", &n3);
 
-    if(numero >= 0){
-        printf("Numero e positivo. ");
-    } else {
-        printf("Numero e negativo. ");
+   float maior = n1;
 
+   if (n2 > maior) {
+        maior = n2;
     }
-    return 0; 
+
+    if (n3 > maior) {
+        maior = n3;
+    }
+
+    printf("O maior número digitado é: %.2f\n", maior);
+
+    return 0;
 }
