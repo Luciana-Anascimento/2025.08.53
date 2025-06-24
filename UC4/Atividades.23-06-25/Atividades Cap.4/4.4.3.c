448a47583e2672f776e69e9a55260d158c1455c1
Luciana-Anascimento <lucianna.anascimento@gmail.com>
2025-06-24 12:09:09 -0300

    Alt 4.4.3.c

---
+++ UC4/Atividades.23-06-25/Atividades Cap.4/4.4.3.c
@@ -0,0 +1,25 @@
+#include <stdio.h> 
+
+int main() {
+    double valorCompra; 
+    double somaTotal = 0.0; 
+
+    printf("Digite os valores das compras (digite 0 para encerrar e ver o total):\n");
+
+    do {
+        printf("Valor da compra: ");
+        scanf("%lf", &valorCompra); 
+
+        if (valorCompra < 0) {
+            printf("Valor invalido! Por favor, digite um valor nao negativo.\n");
+        } else {
+            somaTotal += valorCompra; 
+        }
+
+    } while (valorCompra != 0); 
+
+    printf("\n--- Total das Compras ---\n");
+    printf("A soma total dos valores das compras e: R$ %.2f\n", somaTotal);
+
+    return 0; 
+}
