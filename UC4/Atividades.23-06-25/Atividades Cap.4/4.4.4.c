c7e81419415948f60995168b6f3cc26477dfe73e
Luciana-Anascimento <lucianna.anascimento@gmail.com>
2025-06-26 22:33:37 -0300

    Update 4.4.4.c

--- UC4/Atividades.23-06-25/Atividades Cap.4/4.4.4.c
+++ UC4/Atividades.23-06-25/Atividades Cap.4/4.4.4.c
@@ -4,11 +4,11 @@ int main() {
     int numero; 
 
     do {
-        printf("Digite um número inteiro de 1 a 10 para ver sua tabuada: ");
+        printf("Digite um numero inteiro de 1 a 10 para ver sua tabuada: ");
         scanf("%d", &numero); 
 
         if (numero < 1 || numero > 10) {
-            printf("Número inválido! Por favor, digite um número entre 1 e 10.\n");
+            printf("Numero invalido! Por favor, digite um numero entre 1 e 10.\n");
         }
     } while (numero < 1 || numero > 10); 
 
